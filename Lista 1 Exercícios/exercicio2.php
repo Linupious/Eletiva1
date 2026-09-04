b809bb64181fb097d69c08de16671ae6d596e749
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-03 21:24:31 -0300

    Fiz o exercicio 2

---
+++ Lista 1 Exercícios/exercicio2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2 - Subtrair</title>
+</head>
+<body>
+    <h1>Exercício 2 - Subtrair</h1>
+    <form method="POST" action="resposta1.php">
+        <label for="n1">Informe o primeiro número: </label>
+        <input type="number" id="n1" name="n1">
+        <br>
+        <label for="n2">Informe o segundo número: </label>
+        <input type="number" id="n2" name="n2">
+
+        <button type="submit" class="btn btn-primary">Subtrair</button>
+    </form>
+
+</body>
+</html>
