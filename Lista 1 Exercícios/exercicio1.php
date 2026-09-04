011e294fdab5a51979955595ccfbbd5b6d9d1071
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-03 21:16:15 -0300

    Fiz o exercicio 1

---
+++ Lista 1 Exercícios/exercicio1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1 - Soma</title>
+</head>
+<body>
+    <h1>Exercício 1 - Soma</h1>
+    <form method="POST" action="resposta1.php">
+        <label for="n1">Informe o primeiro número: </label>
+        <input type="number" id="n1" name="n1">
+        <br>
+        <label for="n2">Informe o segundo número: </label>
+        <input type="number" id="n2" name="n2">
+
+        <button type="submit" class="btn btn-primary">Somar</button>
+    </form>
+
+</body>
+</html>
