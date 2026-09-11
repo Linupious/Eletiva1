519676d3969c5d7e89808b9341b169b6094cc090
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-10 21:48:56 -0300

    Fiz exercicio 7

---
+++ Lista 1 Exercícios/exercicio7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 7 - Calcular Área Retângulo</title>
+</head>
+<body>
+<h1>Exercicio 7 - Calcular Área Retângulo</h1>
+    <form method="POST" action="resposta7.php">
+        <label for="base">Informe a base do retângulo: </label>
+        <input type="number" id="base" name="base">
+        <br>
+        <label for="altura">Informe a altura do retângulo: </label>
+        <input type="number" id="altura" name="altura">
+        <br>
+
+        <button type="submit" class="btn btn-primary">Calcular Área</button>
+    </form>
+</body>
+</html>
---
+++ Lista 1 Exercícios/resposta7.php
@@ -0,0 +1,5 @@
+<?php
+    $base = $_POST['base'];
+    $altura = $_POST['altura'];
+    $area = $base * $altura;
+    echo "<p>A área do retângulo é: $area</p>";
