d6272c300f184d44e373b59d59d58022e41b1eb0
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-10 23:00:02 -0300

    Fiz os exercicios 18 e 19

---
+++ Lista 1 Exercícios/exercicio19.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 19 - Calcular Velocidade</title>
+</head>
+<body>
+    <h1>Exercício 19 - Calcular Velocidade</h1>
+    <form method="POST" action="resposta19.php">
+        <label for="distancia">Informe a distância percorrida (em metros): </label>
+        <input type="number" id="distancia" name="distancia">
+        <br>
+
+        <label for="tempo">Informe o tempo gasto (em segundos): </label>
+        <input type="number" id="tempo" name="tempo">
+        <br>
+
+        <button type="submit" class="btn btn-primary">Calcular Velocidade Média</button>
+    </form>
+</body>
+</html>
