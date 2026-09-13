36f9ad7d6356ce3fbba233ad483523a92b23302e
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-13 17:38:41 -0300

    Parte do exercicio 3 da lista 2

---
+++ Lista 2 Exercícios/exercicio3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 3 - Aplicar desconto</title>
+</head>
+<body>
+    <h1>Exercicio 3 - Aplicar desconto</h1>
+    <form method="POST" action="resposta3.php">
+        <label for="valor">Digite o valor do produto: </label>
+        <input type="number" name="valor" id="">
+        <br>
+        
+        <button type="submit" class="btn btn-primary">Aplicar desconto</button>
+</body>
+</html>
