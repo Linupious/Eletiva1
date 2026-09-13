7100c05fe6e65f74e4111cf224428f8474e6e708
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-13 18:09:21 -0300

    Fiz exercicio 4

---
+++ Lista 2 Exercícios/exercicio4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 4 - Exibir Mês</title>
+</head>
+<body>
+    <h1>Exercicio 4 - Exibir Mês</h1>
+    <form method="POST" action="resposta4.php">
+        <label for="mes">Digite o número do mês (1-12): </label>
+        <input type="number" name="mes" id="mes">
+        <br>
+        
+        <button type="submit" class="btn btn-primary">Exibir Mês</button>
+</body>
+</html>
