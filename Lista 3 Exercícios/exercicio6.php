6019c4d73c87eadf5e217fb2bd5cb0440d8457e5
Gabriel V <gabrielvenancio13062006@gmail.com>
2026-09-14 14:28:41 -0300

    Fiz exercicio 6

---
+++ Lista 3 Exercícios/exercicio6.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 6 - Arredondamento</title>
+</head>
+<body>
+    <h1>Exercicio 6 - Arredondamento</h1>
+    <form method="POST" action="resposta6.php">
+        <label for="numero">Digite um número quebrado: </label>
+        <input type="number" name="numero" id="numero" step="0.01">
+        <br>
+        
+        <button type="submit" class="btn btn-primary">Arredondar</button>
+    </form>
+</body>
+</html>
